4de326440bfcc66160bace75a8c3c2feafdcbac2
maddiestrange <maddie.strange@gmail.com>
2019-07-15 15:23:48 -0500

    added onclick functionality to buttons, MVP complete

--- src/App.js
+++ src/App.js
@@ -32,12 +32,12 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={() => sethomeScore(homeScore+7)} className="homeButtons__touchdown">Home Touchdown</button> 
+          <button onClick={() => sethomeScore(homeScore+3)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={() => setawayScore(awayScore+7)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={() => setawayScore(awayScore+3)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
       </section>
     </div>
